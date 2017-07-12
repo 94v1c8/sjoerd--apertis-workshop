1cb5dc17c7217eba4dc26a923254ac8fe5acadd2
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2017-07-12 22:40:38 +0200

    Drop unnecessary sudo usage

--- ospack-sdk/replace-tar-coreutils-for-build.sh
+++ ospack-sdk/replace-tar-coreutils-for-build.sh
@@ -2,6 +2,7 @@
 
 echo "I: replacing GPLv2 tar/coreutils with latest versions used for building software"
 cp /bin/rm /bin/tar /usr/local/bin
-sudo apt-get -y install coreutils tar
-sudo rm /usr/local/bin/rm /usr/local/bin/tar
+apt-get update
+apt-get -y install coreutils tar
+rm /usr/local/bin/rm /usr/local/bin/tar
 
